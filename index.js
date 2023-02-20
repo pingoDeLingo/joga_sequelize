8e9dae27b4f6f83fa2babca2dc627711cac1c266
Robin <robin.noormets@voco.ee>
2023-02-20 11:44:08 +0200

    Created connection to database

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize');
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the application.' });
