8e111d1e21ba6414b95c75f00c43bce578daa47f
Robin <robin.noormets@voco.ee>
2023-02-21 13:46:28 +0200

    Created article table and added table data

---
+++ migrations/20230221111924-modify_author_fields.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return Promise.all([
+      queryInterface.changeColumn('Authors', 'id', { type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      }),
+        queryInterface.changeColumn('Authors', 'name', { type: Sequelize.STRING, allowNull: false })
+    ])
+
+  },
+
+  async down (queryInterface) {
+    return Promise.all([
+      queryInterface.dropTable('Articles')
+    ])
+  }
+};
